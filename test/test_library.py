e9a1c59070dd1d2173ea4a8d63babe06cf81cdfc
nzdjb <github@2a1b.com>
2023-03-11 08:41:12 +0000

    test: Add sample parse test.

--- test/test_library.py
+++ test/test_library.py
@@ -10,3 +10,8 @@ class TestLibrary(TestCase):
     def test_parse_single(self) -> None:
         library = Library('[{"abc":{"tpkd_dict":{"all_tpks":[{"Name": "Test"}]}}}]')
         self.assertEqual(library._library, [{"Name": "Test"}])
+
+    def test_parse_sample(self) -> None:
+        with open("test/files/sample.json") as s:
+            library = Library(s.read())
+        self.assertEqual(len(library._library), 8)
